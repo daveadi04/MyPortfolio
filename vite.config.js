cd71783f2cb81a6f35538403bcc2c98d64d6e753
Aditya dave <Aditya.dave@sheridancollege.ca>
2025-08-19 23:40:10 -0400

    just installed till npm install @radix-ui/react-toast class-variance-authority clsx

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' 
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react() , tailwindcss()],
+ //The resolve property in Vite lets you define path aliases, like '@' for src, to simplify imports, improve readability, and avoid long relative paths.
+  resolve: {
+    alias: {
+      '@': Path.resolve(__dirname, './src'),
+    },
+  },  
 })
